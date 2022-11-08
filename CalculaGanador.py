c92825b7b90f4ee96c0784a05ffe325ac1d79f82
cpaztrillo@gmail.com <cpaztrillo@gmail.com>
2022-11-07 19:13:59 -0500

    test case y comentarios

--- CalculaGanador.py
+++ CalculaGanador.py
@@ -2,7 +2,8 @@ import csv
 # el programa deberá calcular el ganador de votos validos considerando que los siguientes datos son proporcionados:
 # region,provincia,distrito,dni,candidato,esvalido
 # Si hay un candidato con >50% de votos válidos retornar un array con un string con el nombre del ganador
-# Si no hay un candidato que cumpla la condicion anterior, retornar un array con los dos candidatos que pasan a segunda vuelta
+# Si no hay un candidato que cumpla la condicion anterior, retornar un array con los dos candidatos
+# que pasan a segunda vuelta
 # Si ambos empatan con 50% de los votos se retorna el que apareció primero en el archivo
 # el DNI debe ser valido (8 digitos)
 class CalculaGanador:
@@ -18,22 +19,31 @@ class CalculaGanador:
 
     def calcularganador(self, data):
         votosxcandidato = {}
+        votosvalidos = 0
+        # Calcular los votos
         for fila in data:
             if not fila[4] in votosxcandidato:
                 votosxcandidato[fila[4]] = 0
             if fila[5] == '1':
                 votosxcandidato[fila[4]] = votosxcandidato[fila[4]] + 1
+                votosvalidos = votosvalidos + 1
+        #for candidato in votosxcandidato:
+        #    print('candidato: ' + candidato + ' votos validos: ' + str(votosxcandidato[candidato]))
+        # Seleccionar el que tiene mas votos
+        maxvotos = 0
+        candidatoganador = ''
         for candidato in votosxcandidato:
-            print('candidato: ' + candidato + ' votos validos: ' + str(votosxcandidato[candidato]))
-        for candidato in votosxcandidato:
-            return [candidato]
+            if votosxcandidato[candidato] > maxvotos:
+                candidatoganador = candidato
+                maxvotos = votosxcandidato[candidato]
+        return [candidatoganador]
 
 c = CalculaGanador()
 #c.calcularvotos(c.leerdatos())
 datatest = [
-['Áncash', 'Asunción', 'Acochaca', '40810062', 'Eddie Hinesley', '0'],
+['Áncash', 'Asunción', 'Acochaca', '40810062', 'Eddie Hinesley', '1'],
 ['Áncash', 'Asunción', 'Acochaca', '57533597', 'Eddie Hinesley', '1'],
 ['Áncash', 'Asunción', 'Acochaca', '86777322', 'Aundrea Grace', '1'],
 ['Áncash', 'Asunción', 'Acochaca', '23017965', 'Aundrea Grace', '1']
 ]
-print(c.calcularganador(datatest))
+#print(c.calcularganador(datatest))
--- TestCalculaGanador.py
+++ TestCalculaGanador.py
@@ -3,12 +3,18 @@ from CalculaGanador import *
 
 class TestStringMethods(unittest.TestCase):
     def test_ganador(self):
-        datatest = [ ['Áncash', 'Asunción', 'Acochaca', '40810062', 'Eddie Hinesley', '0'] ]
+        datatest = [ ['Áncash', 'Asunción', 'Acochaca', '40810062', 'Eddie Hinesley', '1'] ]
         c = CalculaGanador()
         self.assertEqual(c.calcularganador(datatest), ['Eddie Hinesley'])
 
     def test_ganadorvotoinvalido(self):
         datatest = [ ['Áncash', 'Asunción', 'Acochaca', '40810062', 'Eddie Hinesley', '0'],
+        ['Áncash', 'Asunción', 'Acochaca', '86777322', 'Aundrea Grace', '1']]
+        c = CalculaGanador()
+        self.assertEqual(c.calcularganador(datatest), ['Aundrea Grace'])
+
+    def test_ganadordniinvalido(self):
+        datatest = [ ['Áncash', 'Asunción', 'Acochaca', '4081006', 'Eddie Hinesley', '1'],
         ['Áncash', 'Asunción', 'Acochaca', '86777322', 'Aundrea Grace', '1']]
         c = CalculaGanador()
         self.assertEqual(c.calcularganador(datatest), ['Aundrea Grace'])
